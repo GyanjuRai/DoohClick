32dcbc9064195c5bda5d26dfd1494090a304a763
gyanjurai <raigyanju3@gmail.com>
2026-04-23 22:01:57 +0545

    add contact fields to tenant, uuid to user, and new tables for campaign_playlist and playlist_item

--- database/scripts/create.sql
+++ database/scripts/create.sql
@@ -21,9 +21,9 @@ CREATE TABLE [identity].tenant
 	[name]              NVARCHAR(100) NOT NULL,
 	normalized_name     NVARCHAR(100) NOT NULL UNIQUE,
 	default_currency    NVARCHAR(10) NOT NULL,
+    contact_name        NVARCHAR(50) NOT NULL,
     contact_email       NVARCHAR(100) NOT NULL,
     contact_phone       NVARCHAR(20) NOT NULL,
-    contact_name        NVARCHAR(50) NOT NULL,
     time_zone           NVARCHAR(50) NOT NULL,
     is_active           BIT NOT NULL DEFAULT 1,
 	is_deleted          BIT NOT NULL DEFAULT 0,
@@ -41,6 +41,7 @@ CREATE TABLE [identity].tenant
 CREATE TABLE [identity].[user]
 (
 	id                      INT PRIMARY KEY IDENTITY(1,1),
+	uuid					UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
 	tenant_id               INT NOT NULL REFERENCES [identity].tenant(id),
 	[user_name]             NVARCHAR(100) NOT NULL,
 	normalized_user_name    NVARCHAR(100) NOT NULL,
@@ -89,13 +90,17 @@ CREATE TABLE inv.screen
 	city				NVARCHAR(50) NOT NULL,
 	timezone			NVARCHAR(50) NOT NULL,
 	is_active			BIT NOT NULL DEFAULT 1,
-
+	rate_per_hour		DECIMAL(10,2) NOT NULL,
+	currency			NVARCHAR(50) NOT NULL,
+	
 	created_by          INT NOT NULL REFERENCES [identity].[user](id),
-	created_at          DATETIME NOT NULL,
+	created_at          DATETIME2 NOT NULL,
 	updated_by          INT NULL REFERENCES [identity].[user](id),
-	updated_at          DATETIME NULL,
+	updated_at          DATETIME2 NULL,
 	deleted_by          INT NULL REFERENCES [identity].[user](id),
-	deleted_at          DATETIME NULL
+	deleted_at          DATETIME2 NULL,
+
+	UNIQUE (tenant_id, screen_code)
 );
 
 CREATE TABLE inv.screen_supported_media
@@ -103,12 +108,9 @@ CREATE TABLE inv.screen_supported_media
 	id				INT	PRIMARY KEY IDENTITY(1,1),
 	screen_id		INT NOT NULL REFERENCES inv.screen(id),
 	media_type		NVARCHAR(50) NOT NULL,
-	is_deleted		BIT NOT NULL DEFAULT 0,
 
 	created_by		INT NOT NULL REFERENCES [identity].[user](id),
-	created_at		DATETIME2 NOT NULL,
-	deleted_by		INT NULL REFERENCES [identity].[user](id),
-	deleted_at		DATETIME2 NULL
+	created_at		DATETIME2 NOT NULL
 );
 
 CREATE TABLE inv.screen_operating_hour
@@ -118,14 +120,12 @@ CREATE TABLE inv.screen_operating_hour
 	day_of_week				NVARCHAR(50) NOT NULL,
 	open_time				TIME NOT NULL,
 	close_time				TIME NOT NULL,
+	rate_per_hr				DECIMAL(10,2) NOT NULL,
 	audience_source			NVARCHAR(50) NOT NULL,
 	estimated_impression	INT NULL,
-	is_deleted				BIT NOT NULL DEFAULT 0,
 
 	created_by				INT NOT NULL REFERENCES [identity].[user](id),
-	created_at				DATETIME2 NOT NULL,
-	deleted_by				INT NULL REFERENCES [identity].[user](id),
-	deleted_at				DATETIME2 NULL
+	created_at				DATETIME2 NOT NULL
 );
 
 CREATE TABLE crm.advertiser
@@ -156,9 +156,9 @@ CREATE TABLE campaign
 	advertiser_id	INT NOT NULL REFERENCES crm.advertiser(id),
 	[name]			NVARCHAR(100) NOT NULL,
 	campaign_code	NVARCHAR(40) NOT NULL,
+	[status]		NVARCHAR(50) NOT NULL,  -- DRAFT SCHEDULED  CANCELLED  ACTIVE  COMPLETED
 	[start_date]	DATE	NOT NULL,
-	end_date	DATE NOT NULL,
-	[status]		NVARCHAR(50) NOT NULL,
+	end_date		DATE NOT NULL,
 	note			NVARCHAR(255) NULL,
 	is_deleted		BIT DEFAULT 0,
 
@@ -167,21 +167,29 @@ CREATE TABLE campaign
 	updated_by		INT  NULL REFERENCES [identity].[user](id),
 	updated_at		DATETIME2 NULL,
 	deleted_by		INT NULL REFERENCES [identity].[user](id),
-	deleted_at		DATETIME2 NULL
+	deleted_at		DATETIME2 NULL,
+
+	UNIQUE (tenant_id, campaign_code)
 );
 
 CREATE TABLE campaign_schedule
 (
-	id					INT PRIMARY KEY IDENTITY(1,1),
-	campaign_id			INT NOT NULL REFERENCES campaign(id),
-	start_date_time		DATETIME2 NOT NULL,
-	end_date_time		DATETIME2 NOT NULL,
-	is_deleted			BIT NOT NULL DEFAULT 0,
+	id						INT PRIMARY KEY IDENTITY(1,1),
+	campaign_id				INT NOT NULL REFERENCES campaign(id),
+	screen_id				INT NOT NULL REFERENCES inv.screen(id),
+	start_date_time			DATETIME2 NOT NULL,
+	end_date_time			DATETIME2 NOT NULL,
+	rate_per_hour			DECIMAL(10,2) NOT NULL,
+    total_hours             AS (CAST(DATEDIFF(MINUTE, start_date_time, end_date_time) / 60.0 AS DECIMAL(10,2))) PERSISTED,
+    total_amount            AS (CAST(DATEDIFF(MINUTE, start_date_time, end_date_time) / 60.0 * rate_per_hour AS DECIMAL(10,2))) PERSISTED,
+    currency                NVARCHAR(10) NOT NULL,
+    estimated_impressions   INT NULL,           -- informational only
+	is_deleted				BIT NOT NULL DEFAULT 0,
 
-	created_by			INT			NOT NULL REFERENCES [identity].[user](id),
-    created_at			DATETIME2	NOT NULL,
-    deleted_by			INT    NULL REFERENCES [identity].[user](id),
-    deleted_at			DATETIME2           NULL
+	created_by				INT			NOT NULL REFERENCES [identity].[user](id),
+    created_at				DATETIME2	NOT NULL,
+    deleted_by				INT    NULL REFERENCES [identity].[user](id),
+    deleted_at				DATETIME2           NULL
 );
 
 CREATE TABLE media_library
@@ -203,28 +211,56 @@ CREATE TABLE media_library
 	created_by		INT NOT NULL REFERENCES [identity].[user](id),
 	created_at		DATETIME2 NOT NULL,
 	deleted_by		INT NULL REFERENCES [identity].[user](id),
-	deleted_at		DATETIME2 NULL
+	deleted_at		DATETIME2 NULL,
+
+	UNIQUE (tenant_id, display_name)
 );
 
-CREATE TABLE campaign_screen_media
+CREATE TABLE campaign_playlist
 (
-	id				INT PRIMARY KEY IDENTITY(1,1),
-	campaign_id		INT NOT NULL,
-	media_id		INT NOT NULL,
-	screen_id		INT NOT NULL,
-	play_sequence	INT NOT NULL
+	id						INT PRIMARY KEY IDENTITY(1,1),
+	campaign_schedule_id	INT NOT NULL REFERENCES campaign_schedule(id),
+	[name]					NVARCHAR(100) NOT NULL,
+	is_active				BIT NOT NULL DEFAULT 1,
+	[version]				INT NOT NULL DEFAULT 1,
+	is_deleted				BIT NOT NULL DEFAULT 0,
+
+	created_by				INT NOT NULL REFERENCES [identity].[user](id),
+    created_at				DATETIME2 NOT NULL,
+    updated_by				INT NULL REFERENCES [identity].[user](id),
+    updated_at				DATETIME2 NULL,
+    deleted_by				INT NULL REFERENCES [identity].[user](id),
+    deleted_at				DATETIME2 NULL
+);
+
+CREATE TABLE playlist_item
+(
+	id                    INT PRIMARY KEY IDENTITY(1,1),
+    playlist_id           INT NOT NULL REFERENCES campaign_playlist(id),
+    media_id              INT NOT NULL REFERENCES media_library(id),
+    play_sequence         INT NOT NULL,
+    duration_override_sec INT NULL,
+    is_deleted            BIT NOT NULL DEFAULT 0,
+
+    created_by            INT NOT NULL REFERENCES [identity].[user](id),
+    created_at            DATETIME2 NOT NULL,
+    deleted_by            INT NULL REFERENCES [identity].[user](id),
+    deleted_at            DATETIME2 NULL,
+
+    CONSTRAINT uq_playlist_sequence UNIQUE (playlist_id, play_sequence)
 );
 
 CREATE TABLE shared.audit_log
 (
-	id BIGINT IDENTITY,
-	entity_type NVARCHAR(50) NOT NULL,
-	[entity_id] UNIQUEIDENTIFIER NOT NULL,
-	[action] NVARCHAR(20) NOT NULL,
-	changed_by UNIQUEIDENTIFIER NOT NULL,
-	changed_at DATETIME NOT NULL DEFAULT GETUTCDATE(),
-	old_value NVARCHAR(MAX) NULL,
-	new_value NVARCHAR(MAX) NULL
+	id					INT PRIMARY KEY IDENTITY,
+	tenant_id			INT NOT NULL REFERENCES [identity].tenant(id),
+	entity_type			NVARCHAR(50) NOT NULL,
+	[entity_id]			UNIQUEIDENTIFIER NOT NULL,
+	[action]			NVARCHAR(20) NOT NULL,
+	changed_by			INT NOT NULL REFERENCES [identity].[user](id),
+	changed_at			DATETIME NOT NULL DEFAULT GETUTCDATE(),
+	old_value			NVARCHAR(MAX) NULL,
+	new_value			NVARCHAR(MAX) NULL
 );
 
 CREATE TABLE shared.tag
@@ -240,22 +276,22 @@ CREATE TABLE shared.tag
     updated_at			DATETIME2	NULL
 );
 
-CREATE TABLE shared.list_item_category
+CREATE TABLE shared.listitem_category
 (
 	id INT PRIMARY KEY IDENTITY(1,1),
-    tenant_id UNIQUEIDENTIFIER NULL,
+    tenant_id INT NULL REFERENCES [identity].tenant(id),
 	category NVARCHAR(100) NOT NULL,
 	category_code NVARCHAR(50) NOT NULL
 );
 
-CREATE TABLE shared.list_item
+CREATE TABLE shared.listitem
 (
 	id INT PRIMARY KEY IDENTITY(1,1),
-    tenant_id UNIQUEIDENTIFIER NULL,
+    tenant_id INT NULL REFERENCES [identity].tenant(id),
 	category_id INT NOT NULL,
 	item NVARCHAR(100) NOT NULL,
 	code NVARCHAR(50) NOT NULL,
 	is_active BIT NOT NULL DEFAULT 1,
 	
-	FOREIGN KEY (category_id) REFERENCES shared.list_item_category(id)
+	FOREIGN KEY (category_id) REFERENCES shared.listitem_category(id)
 );

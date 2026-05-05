0bda25bfb2762547e0a5adaabf8add77332c6f40
gyanjurai <raigyanju3@gmail.com>
2026-05-06 00:51:07 +0545

    Add logging and try catch in OrphanFileCleanupJob.

--- DoohClick.API/DoohClick.API/Worker/OrphanFileCleanupJob.cs
+++ DoohClick.API/DoohClick.API/Worker/OrphanFileCleanupJob.cs
@@ -6,28 +6,41 @@ namespace DoohClick.API.worker
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
         private readonly IWebHostEnvironment _webHostEnvironment;
-
+        private readonly ILogger _logger;
         public OrphanFileCleanupJob(
                 IServiceScopeFactory serviceScopeFactory,
-                IWebHostEnvironment webHostEnvironment
+                IWebHostEnvironment webHostEnvironment,
+                ILogger<OrphanFileCleanupJob> logger
             )
         {
             _serviceScopeFactory = serviceScopeFactory;
             _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(UnitNextRun(), stoppingToken);
-                await DoCleanUp();
+                await Task.Delay(GetNextRunDelay(), stoppingToken);
+                try
+                {
+                    await DoCleanUp();
+                    _logger.LogInformation("DoCleanUp completed at {Time}", DateTime.UtcNow);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "DoCleanUp failed at {Time}", DateTime.Now);
+                }
             }
         }
 
-        private static TimeSpan UnitNextRun()
+        private static TimeSpan GetNextRunDelay()
         {
-            return TimeSpan.FromMinutes(30);
+            DateTime now = DateTime.Now;
+            DateTime next = now.Date.AddDays(1).AddHours(2);
+            return next - now;
         }
 
         private async Task DoCleanUp()
